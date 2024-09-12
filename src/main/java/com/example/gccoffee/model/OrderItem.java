b6144e561e4582175efeb5585911ccbdfd55cf89
yechan <rladPcks980@gmail.com>
2024-09-12 10:19:35 +0900

    feat:OrderItem 조회시 productName 추가

--- src/main/java/com/example/gccoffee/model/OrderItem.java
+++ src/main/java/com/example/gccoffee/model/OrderItem.java
@@ -2,5 +2,5 @@ package com.example.gccoffee.model;
 
 import java.util.UUID;
 
-public record OrderItem(UUID productId, Category category, long price, int quantity) {
+public record OrderItem(UUID productId,String productName, Category category, long price, int quantity) {
 }
--- src/main/java/com/example/gccoffee/repository/OrderItemJdbcRepository.java
+++ src/main/java/com/example/gccoffee/repository/OrderItemJdbcRepository.java
@@ -13,20 +13,25 @@ import java.util.UUID;
 public class OrderItemJdbcRepository implements OrderItemRepository{
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final ProductRepository productRepository;
 
-    public OrderItemJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+    public OrderItemJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate, ProductRepository productRepository) {
         this.jdbcTemplate = jdbcTemplate;
+        this.productRepository = productRepository;
     }
 
     @Override
     public List<OrderItem> findByOrderId(UUID orderId) {
         return jdbcTemplate.query(
-                "SELECT BIN_TO_UUID(product_id) as product_id, category, price, quantity " +
-                        "FROM order_items WHERE order_id = UUID_TO_BIN(:orderId)",
+                "SELECT BIN_TO_UUID(o.product_id) as product_id, p.product_name, o.category, o.price, o.quantity " +
+                        "FROM order_items o " +
+                        "JOIN products p ON o.product_id = p.product_id " +
+                        "WHERE o.order_id = UUID_TO_BIN(:orderId)",
                 Map.of("orderId", orderId.toString().getBytes()),
                 (rs, rowNum) -> new OrderItem(
                         UUID.fromString(rs.getString("product_id")),
-                        Category.valueOf(rs.getString("category")), // Category는 적절히 변환해줘야 함
+                        rs.getString("product_name"),
+                        Category.valueOf(rs.getString("category")),
                         rs.getLong("price"),
                         rs.getInt("quantity")
                 )
